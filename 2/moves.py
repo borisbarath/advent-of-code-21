cf3ecde6f737e27408f0616bb637c03576d9f30c
Boris Barath <boris.barath@gmail.com>
2021-12-02 19:16:31 +0000

    day 2

--- 2/moves.py
+++ 2/moves.py
@@ -4,13 +4,15 @@ with open(os.path.join(os.path.dirname(__file__), "input.txt"), 'r') as input:
     commands = [command.split() for command in input.readlines()]
     depth = 0
     horizontal = 0
+    aim = 0
     for (cmd, dist) in commands:
         distance = int(dist)
         if cmd == "forward":
             horizontal += distance
+            depth += aim * distance
         elif cmd == "up":
-            depth -= distance
+            aim -= distance
         elif cmd == "down":
-            depth += distance
-    print("depth: {}, horizontal: {}".format(depth, horizontal))
+            aim += distance
+    print("depth: {}, horizontal: {}, aim: {}".format(depth, horizontal, aim))
     print(depth * horizontal)
